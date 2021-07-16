e7030bda2eb240696a39c4b2c93728ec7f6650ef
Radek Skrabucha <rskrabucha13@gmail.com>
2021-07-16 19:45:07 +0200

    fixed fetching bug

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
 	return (
 		<header>
 			<h2>This is header</h2>
+			<Link to="/">Home</Link>
+			<Link to="/article/1">Article</Link>
 		</header>
 	);
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+	const [posts, setPosts] = useState([]);
+	useEffect(async () => {
+		const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+		const data = await response.json();
+		console.log(data);
+		setPosts(data);
+	}, []);
 	return (
 		<div>
 			<h1>Hello from home page!</h1>
